1f27d26797ee5bbe32fd0c4c436e746dbffb524e
Siddharth Rao <siddrao.926@gmail.com>
2024-12-02 18:57:00 -0800

    Defined Database Models

---
+++ apps/backend/src/backend/models/db/event.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from pydantic import BaseModel, field_validator
+
+
+class Event(BaseModel):
+    key: str
+    name: str
+    event_code: str
+    event_type: str
+    district: Optional[str] = None
+    city: Optional[str] = None
+    state_prov: Optional[str] = None
+    country: Optional[str] = None
+    start_date: str
+    end_date: str
+    year: int
---
+++ apps/backend/src/backend/models/db/tba_page_etag.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+
+class TBAPageEtag(BaseModel):
+    page_num: int
+    etag: str
+    endpoint: str
