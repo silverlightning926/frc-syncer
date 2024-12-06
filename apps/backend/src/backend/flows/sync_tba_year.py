d264dd31be6ce36fb7f52716a3123efc5cf6a567
Siddharth Rao <siddrao.926@gmail.com>
2024-12-05 19:43:17 -0800

    Added Throttling Between Years

--- apps/backend/src/backend/flows/sync_tba_year.py
+++ apps/backend/src/backend/flows/sync_tba_year.py
@@ -1,7 +1,14 @@
-from prefect import flow
+from time import sleep
+
+from prefect import flow, task
 from tasks.sync_event_matches import sync_all_event_matches
 from tasks.sync_events import fetch_events
 from tasks.sync_teams import fetch_teams
+
+
+@task
+def throttle_request(interval_secs=60):
+    sleep(interval_secs)
 
 
 @flow(
@@ -15,3 +22,5 @@ def sync_tba_data_for_year(year: int):
     fetch_events(year=year)
 
     sync_all_event_matches(year=year)
+
+    throttle_request()
