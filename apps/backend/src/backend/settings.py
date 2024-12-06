23b9e9db2f6f7e9f783b128753b83fe0874b02fc
Siddharth Rao <siddrao.926@gmail.com>
2024-12-05 19:26:35 -0800

    Adding Years To Historic Data Download

--- apps/backend/src/backend/settings.py
+++ apps/backend/src/backend/settings.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
 
-    historic_seasons: list[int] = [2016, 2017, 2018, 2019, 2022, 2023, 2024]
+    historic_seasons: list[int] = [2016, 2017, 2018, 2019, 2020, 2022, 2023, 2024]
 
     sync_delta: timedelta = timedelta(weeks=1)
 
