be49910f5fcf7797b3096927f9ef4ed44b309da0
Siddharth Rao <siddrao.926@gmail.com>
2024-12-09 18:08:11 -0800

    Created API and Processor Projects

---
+++ apps/api/src/api/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+from .routers import status
+
+app = FastAPI()
+
+app.include_router(status.router)
---
+++ apps/api/src/api/services/db_services.py
@@ -0,0 +1,16 @@
+from supabase import Client, create_client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("SUPABASE_URL and SUPABASE_KEY must be set")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def get_supabase():
+    return supabase
